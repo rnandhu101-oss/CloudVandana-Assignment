281327ce97e854e36fc01ecacbadf82d1b42cf09
rnandhu101-oss <rnandhu101@gmail.com>
2026-06-16 12:58:16 +0530

    fixed login redirect

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,13 @@
 import { FaSalesforce } from "react-icons/fa";
 
 const Login = () => {
-
   const handleLogin = () => {
     window.location.href =
-    `${import.meta.env.VITE_BACKEND_URL}/auth/login`;
+      `${import.meta.env.VITE_BACKEND_URL}/auth/login`;
   };
 
   return (
-    <button
-      className="login-btn"
-      onClick={handleLogin}
-    >
-      <FaSalesforce />
+    <button onClick={handleLogin}>
       Login with Salesforce
     </button>
   );
